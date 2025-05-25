05e04dd1595ef677cf4a9b138913c474c680e98f
ardasher-kh <_>
2025-05-25 23:08:18 +0500

    autocomplete keymap rework and snippet jump

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jk", "<ESC>", { silent = true })
+vim.keymap.set({ "s", "i" }, "<C-l>", function()
+  return vim.snippet.active({ direction = 1 }) and "<cmd>lua vim.snippet.jump(1)<cr>" or "<Tab>"
+end, { expr = true, desc = "Jump Next" })
+vim.keymap.set({ "i", "s" }, "<C-h>", function()
+  return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<S-Tab>"
+end, { expr = true, desc = "Jump Previous" })
--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,32 +1,19 @@
-local has_words_before = function()
-  local col = vim.api.nvim_win_get_cursor(0)[2]
-  if col == 0 then
-    return false
-  end
-  local line = vim.api.nvim_get_current_line()
-  return line:sub(col, col):match("%s") == nil
-end
 return {
   "saghen/blink.cmp",
   opts = {
     keymap = {
       preset = "enter",
-
-      -- If completion hasn't been triggered yet, insert the first suggestion; if it has, cycle to the next suggestion.
       ["<Tab>"] = {
-        function(cmp)
-          if has_words_before() then
-            return cmp.insert_next()
-          end
-        end,
+        "select_next",
+        "snippet_forward",
         "fallback",
       },
-      -- Navigate to the previous suggestion or cancel completion if currently on the first one.
-      ["<S-Tab>"] = { "insert_prev" },
-    },
-    completion = {
-      menu = { enabled = true },
-      list = { selection = { preselect = false }, cycle = { from_top = false } },
+
+      ["<S-Tab>"] = {
+        "select_prev",
+        "snippet_backward",
+        "fallback",
+      },
     },
   },
 }
